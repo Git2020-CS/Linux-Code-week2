f5f90c51e9f8dc5d96bc93c1fb755a818a095420
Git2020-CS <tjubjy2015@163.com>
2020-01-10 11:28:02 +0800

    add atexit()

--- process/exit/exit.c
+++ process/exit/exit.c
@@ -3,19 +3,26 @@
 	> Author: Name
 	> Mail: Name@163.com 
 	> Created Time: 2020-01-09 18:06:54
+    > 调用exit退出会清理缓冲区，调用_exit退出不会清理缓冲区
  ************************************************************************/
 
 #include <func.h>
+void exitFun1()
+{
+    printf("this is exitFun1\n");
+}
 
 void exitFun()
 {
     printf("exitFun\n");
-    printf("exit print");
+    printf("exit print");//调用exit()退出时会打印这行，调用_exit()则不会打印
     _exit(0);
+    /* exit(0); */
 }
 
 int main()
 {
+    atexit(exitFun1);
     printf("main\n");
     exitFun();
 
